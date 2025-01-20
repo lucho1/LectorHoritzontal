944b2de872a6841bc81a658f80a588012f05385a
Lucho Suaya <luchosuaya@hotmail.com>
2025-01-20 23:11:44 +0100

    Moved functionality from text_viewer.py to serialization.py and ui_globals.py

--- scripts/text_viewer.py
+++ scripts/text_viewer.py
@@ -1,9 +1,11 @@
 import os
 import json
+import serialization
 import app_globals as app
+import ui_globals as ui
 import tkinter as tk
 
-from tkinter import filedialog, ttk, font
+from tkinter import ttk, font
 from file_readers import read_file
 
 
@@ -35,107 +37,55 @@ class TextViewer:
         self.top_frame.pack(fill=tk.X, padx=5, pady=5)
         
         # Create font dropdown
-        self.font_var = tk.StringVar(value=self.current_font)
-        self.font_dropdown = ttk.Combobox(self.top_frame,
-                                        textvariable=self.font_var,
-                                        values=self.fonts,
-                                        state='readonly',
-                                        width=35)
-        
-        self.font_dropdown.pack(side=tk.LEFT, padx=(0, 10))
-        self.font_dropdown.bind('<<ComboboxSelected>>', self.change_font)
+        self.font_dropdown, self.font_var = ui.create_dropdown(self.top_frame, self.fonts, self.current_font, 35, self.change_font, (0, 10))
 
         # Create size dropdown
-        self.size_var = tk.StringVar(value=str(self.font_size))
-        self.size_dropdown = ttk.Combobox(self.top_frame,
-                                        textvariable=self.size_var,
-                                        values=self.sizes,
-                                        state='readonly',
-                                        width=10)
-        self.size_dropdown.pack(side=tk.LEFT)
-        self.size_dropdown.bind('<<ComboboxSelected>>', self.change_font)
+        self.size_dropdown, self.size_var = ui.create_dropdown(self.top_frame, self.sizes, str(self.font_size), 10, self.change_font)
         
         # Create left frame for dropdowns
         self.left_frame = tk.Frame(self.top_frame, bg='#E0E0E0')
         self.left_frame.pack(side=tk.LEFT)
 
         # Add reset button to the right of the speed controls
-        self.reset_button = ttk.Button(self.left_frame, 
-                                     text="Reinicia Valors", 
-                                     command=self.reset_settings,
-                                     takefocus=False)
-        self.reset_button.pack(side=tk.LEFT, padx=(10, 0))
+        self.reset_button = ui.create_button(self.left_frame, "Reinicia Valors", self.reset_settings, False, (10, 0))
         self.root.bind('<Configure>', self.on_window_configure)
         
-         # Create speed slider
-        tk.Label(self.left_frame, text="Velocitat: ", bg='#E0E0E0').pack(side=tk.LEFT, padx=(10, 2))
-        self.speed_slider = ttk.Scale(self.left_frame, 
-                                    from_=1,        # Fastest
-                                    to=50,         # Slowest
-                                    value=self.scroll_amount, # Default value
-                                    orient='horizontal',
-                                    length=100,     # Slider width in pixels
-                                    command=self.update_scroll_speed)
-        self.speed_slider.pack(side=tk.LEFT)
+        # Create speed slider
+        ui.create_label(self.left_frame, "Velocitat: ", (10, 2))
+        self.speed_slider = ui.create_slider(self.left_frame, 1, 50, self.scroll_amount, self.update_scroll_speed)
         
         # Add label to show current speed value
-        self.scroll_label = tk.Label(self.left_frame, 
-                                  text=str(self.scroll_amount)+"%", 
-                                  bg='#E0E0E0',
-                                  width=4)
-        self.scroll_label.pack(side=tk.LEFT)
-        
-
+        self.scroll_label = ui.create_label(self.left_frame, str(self.scroll_amount)+"%")
+        #width=4
 
         # Create center frame for button
         self.center_frame = tk.Frame(self.top_frame, bg='#E0E0E0')
         self.center_frame.pack(expand=True)
+
+        # Add file open button
+        self.open_button = ui.create_button(self.center_frame, "Obrir Arxiu", self.open_new_document)
         
         # Create scroll button
         self.is_scrolling = False
-        
-        # Add file open button
-        self.open_button = ttk.Button(self.center_frame, 
-                                    text="Obrir Arxiu", 
-                                    command=self.open_file,
-                                    takefocus=False)
-        self.open_button.pack(side=tk.LEFT, padx=5)
-
-        self.scroll_left_button = ttk.Button(self.center_frame, text="← Mou", takefocus=False)
-        self.scroll_left_button.pack(side=tk.LEFT, padx=5)
-        
-        self.scroll_right_button  = ttk.Button(self.center_frame, text="Mou →", takefocus=False)
-        self.scroll_right_button.pack(side=tk.LEFT, padx=5)        
+        self.scroll_left_button = ui.create_button(self.center_frame, "← Mou")
+        self.scroll_right_button = ui.create_button(self.center_frame, "Mou →")
 
         # Bind button press and release events
-        self.scroll_left_button.bind('<ButtonPress-1>', lambda e: self.start_scrolling(e, False))
-        self.scroll_left_button.bind('<ButtonRelease-1>', self.stop_scrolling)
-        self.scroll_right_button.bind('<ButtonPress-1>', lambda e: self.start_scrolling(e, True))
-        self.scroll_right_button.bind('<ButtonRelease-1>', self.stop_scrolling)
-
+        ui.bind_button_events(self.scroll_left_button, lambda e: self.start_scrolling(e, False), self.stop_scrolling)
+        ui.bind_button_events(self.scroll_right_button, lambda e: self.start_scrolling(e, True), self.stop_scrolling)
 
         # Create a frame to hold the text
         self.frame = tk.Frame(root, bg='#E0E0E0')
         self.frame.pack(expand=True, fill='both')
         
         # Create horizontal scrollbar
-        self.scrollbar = tk.Scrollbar(self.frame, orient='horizontal')
-        self.scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.scrollbar = ui.create_scrollbar(self.frame, 'horizontal', tk.X)
         
+        # Store text for font changes
+        self.text = text
+
         # Configure text widget
-        self.text_area = tk.Text(self.frame, 
-                                wrap='none',
-                                height=1,
-                                width = 1000,
-                                bg='#E0E0E0', 
-                                fg='black', 
-                                font=('Calibri', 75),
-                                xscrollcommand=self.scrollbar.set)
-        
-        self.text_area.pack(expand=True, fill='x')
-        self.text = text  # Store text for font changes
-        
-        # Configure scrollbar
+        self.text_area = ui.create_textarea(self.frame, 1, 1000, ('Calibri', 75), self.scrollbar.set)
         self.scrollbar.config(command=self.text_area.xview)
         
         # Insert text and disable editing
@@ -229,88 +179,53 @@ class TextViewer:
     
 
     def save_settings(self):
-        settings = {
-            'font': self.font_var.get(),
-            'font_size': int(self.size_var.get()),
-            'speed': int(float(self.speed_slider.get())),
-            'window_width': self.window_width,
-            'window_height': self.window_height,
-            'window_x': self.window_x,
-            'window_y': self.window_y
-        }
-        
-        try:
-            os.makedirs(os.path.dirname(app.SETTINGS_FILEPATH), exist_ok=True)
-            with open(app.SETTINGS_FILEPATH, 'w') as f:
-                json.dump(settings, f)
-        except Exception as e:
-            print(f"Couldn't save settings: {e}")
+        serialization.set_setting('font', self.font_var.get())
+        serialization.set_setting('font_size', int(self.size_var.get()))
+        serialization.set_setting('speed', int(float(self.speed_slider.get())))
+        serialization.set_setting('win_w', self.window_width)
+        serialization.set_setting('win_h', self.window_height)
+        serialization.set_setting('win_x', self.window_x)
+        serialization.set_setting('win_y', self.window_y)
+
+        serialization.save()
     
     def load_settings(self):
-        print("Loading settings...")
-        settings = app.DEFAULT_SETTINGS.copy()
-        
-        try:
-            if os.path.exists(app.SETTINGS_FILEPATH):
-                print(f"Settings loaded from: {app.SETTINGS_FILEPATH}")
-                with open(app.SETTINGS_FILEPATH, 'r') as f:
-                    loaded_settings = json.load(f)
-                    settings.update(loaded_settings)
-            else:
-                print(f"No settings file found at: {app.SETTINGS_FILEPATH}")
-                print("Using default settings")
-        except Exception as e:
-            print(f"Couldn't load settings: {e}")
-            print("Using default settings")
+        serialization.load()
         
         # Store values to be used in UI initialization
-        self.current_font = settings['font']
-        self.font_size = settings['font_size']
-        self.scroll_amount = settings['speed']
-        self.window_width = settings['window_width']
-        self.window_height = settings['window_height']
-        self.window_x = settings['window_x']
-        self.window_y = settings['window_y']
+        self.current_font = serialization.get_setting('font')
+        self.font_size = serialization.get_setting('font_size')
+        self.scroll_amount = serialization.get_setting('speed')
+        self.window_width = serialization.get_setting('window_width')
+        self.window_height = serialization.get_setting('window_height')
+        self.window_x = serialization.get_setting('window_x')
+        self.window_y = serialization.get_setting('window_y')
         print("Settings used: [",
-              f"Font: {settings['font']} |",
-              f"Font Size: {settings['font_size']} |",
-              f"Speed: {settings['speed']} |",
-              f"Window: {settings['window_width']}x{settings['window_height']} @({settings['window_x']},{settings['window_y']})",
+              f"Font: {self.current_font} |",
+              f"Font Size: {self.font_size} |",
+              f"Speed: {self.scroll_amount} |",
+              f"Window: {self.window_width}x{self.window_height} @({self.window_x},{self.window_y})",
               "]")
     
-    def open_file(self):
-        # Ask for file
-        file_types = [
-            ('All supported files', '*.epub;*.pdf;*.docx'),
-            ('EPUB files', '*.epub'),
-            ('PDF files', '*.pdf'),
-            ('Word files', '*.docx'),
-        ]
+    def open_new_document(self):
+        document_path: str = app.open_file_dialog()
+        if not document_path:
+            return
         
-        filepath = filedialog.askopenfilename(
-            title='Select a file to read',
-            filetypes=file_types
-        )
-        
-        if filepath:  # If a file was selected
-            try:
-                # Read new file
-                new_text = read_file(filepath)
-                
-                # Update text area
-                self.text = new_text
-                self.text_area.configure(state='normal')
-                self.text_area.delete(1.0, tk.END)
-                self.text_area.insert(tk.END, new_text)
-                self.text_area.configure(state='disabled')
-                
-                # Reset scroll position
-                self.text_area.xview_moveto(0)
-                
-                # Update window title with filename
-                filename = os.path.basename(filepath)
-                self.root.title(f"Lector Horitzontal - {filename}")
-                
-            except Exception as e:
-                print(f"Error loading file: {e}")
+        try:
+            # Read new file
+            new_text = read_file(document_path)
 
+            self.text = new_text
+            self.text_area.configure(state='normal')
+            self.text_area.delete(1.0, tk.END)
+            self.text_area.insert(tk.END, new_text)
+            self.text_area.configure(state='disabled')
+
+            # Reset scroll position
+            self.text_area.xview_moveto(0)
+            
+            # Update window title with filename
+            self.root.title(f"Lector Horitzontal - {app.get_filename(document_path)}")
+        except Exception as e:
+            print(f"Error loading file: {e}")
